b1dd0384d6fb8273a85f06e0e4a7d0eaee521787
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-09-19 18:56:39 -0700

    Added state for hours and minutes

--- react-time-calculator/src/App.js
+++ react-time-calculator/src/App.js
@@ -10,37 +10,47 @@ function App() {
   }, [])
 
   const [ currentTime, setCurrentTime ] = useState("");
+  const [ hoursInput, setHoursInput ] = useState("");
+  const [ minutesInput, setMinutesInput ] = useState("");
 
   function generateCurrentTime () {
     const timeNow = new Date().toISOString();
     setCurrentTime(timeNow);
   }
 
+  function submitTime(e) {
+      e.preventDefault();
+      console.log("hours: ", hoursInput);
+      console.log("minutes: ", minutesInput);
+  }
+
   return (
     <div className="App">
       <h1>React Time Calculator</h1>
       <div className="currentTime">
-        <h2>Current Time</h2>
-        <h4>{currentTime}</h4>
+          <h2>Current Time</h2>
+          <h4>{currentTime}</h4>
       </div>
-      <div className="timeInput">
-        <div className="hoursInput">
-          <h4>Hours</h4>
-          <input/>
-        </div>
-        <div className="minutesInput">
-          <h4>Minutes</h4>
-          <input/>
-        </div>
-        <p></p>
-        <div className="entryList">
-          <EntryList />
-        </div>
-        <p></p>
-        <div className="addTime">
-          <button>Add Time</button>
-        </div>
-      </div>
+      <form onSubmit={submitTime}>
+          <div className="timeInput">
+            <div className="hoursInput">
+              <h4>Hour(s)</h4>
+              <input onChange={(e) => setHoursInput(e.target.value)} value={hoursInput}/>
+            </div>
+            <div className="minutesInput">
+              <h4>Minutes</h4>
+              <input onChange={(e) => setMinutesInput(e.target.value)} value={minutesInput}/>
+            </div>
+            <p></p>
+            <div className="entryList">
+              <EntryList />
+            </div>
+            <p></p>
+            <div className="addTime">
+              <button>Add Time</button>
+            </div>
+          </div>
+      </form>
     </div>
   );
 }
